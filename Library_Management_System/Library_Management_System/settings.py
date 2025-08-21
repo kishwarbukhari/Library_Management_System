15bb54c3630713d6654122d183d2436460e8da96
Kishwar Bukhari <kishwarbukharii@gmail.com>
2025-08-20 17:51:28 -0700

    Updated

--- Library_Management_System/Library_Management_System/settings.py
+++ Library_Management_System/Library_Management_System/settings.py
@@ -26,8 +26,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'books',
-    'transactions',
 ]
 
 MIDDLEWARE = [
--- Library_Management_System/accounts/models.py
+++ Library_Management_System/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
 
-class User(AbstractUser):
+class User(models.Model):
     is_student = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
     department = models.CharField(max_length=100,blank=True,null=True)
@@ -14,7 +14,7 @@ class Book(models.Model):
         author = models.CharField(max_length=200)
         category = models.CharField(max_length=100)
         isbn = models.CharField(max_length=20, unique=True)
-        satus = models.BooleanFieldd(default=True)
+        satus = models.BooleanField(default=True)
 
         def __str__(self):
             return self.title
@@ -22,7 +22,7 @@ class Book(models.Model):
 
 
 
-class Issue(models.model):
+class Issue(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     issue_date = models.DateField( auto_now_add=True)
